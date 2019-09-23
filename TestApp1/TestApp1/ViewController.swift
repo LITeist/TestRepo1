f8a8db1249a054f4d7018f536c7d341686112bf9
Alexey Levanov <alexeylevanov27@gmail.com>
2019-09-23 17:44:10 +0300

    Первый кормит - initial commit

---
+++ TestApp1/TestApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp1
+//
+//  Created by Alexey Levanov on 23/09/2019.
+//  Copyright © 2019 Alexey Levanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
